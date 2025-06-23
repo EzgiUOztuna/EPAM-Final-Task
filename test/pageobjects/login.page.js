bc0d267707b99f24c60b0241bd233598dcbbbe97
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-23 09:30:21 +0300

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -11,7 +11,7 @@ class LoginPage extends Page {
     get inputUsername() { return $('#user-name'); }
     get inputPassword() { return $('#password'); }
     get btnSubmit() { return $('#login-button'); }
-    get errorMsg() { return $('#login_button_container form div:nth-child(3) h3'); }
+    get errorMsg() { return $('.error-message-container h3'); }
 
     async setUsername(username) {
         await this.inputUsername.setValue(username);
@@ -22,6 +22,53 @@ class LoginPage extends Page {
     }
 
     async clickLogin() {
+        const { $ } = require('@wdio/globals')
+        const Page = require('./page');
+
+        /**
+         * sub page containing specific selectors and methods for a specific page
+         */
+        class LoginPage extends Page {
+            /**
+             * define selectors using getter methods
+             */
+            get inputUsername() { return $('#user-name'); }
+            get inputPassword() { return $('#password'); }
+            get btnSubmit() { return $('#login-button'); }
+            get errorMsg() { return $('.error-message-container'); }
+
+            async setUsername(username) {
+                await this.inputUsername.setValue(username);
+            }
+
+            async setPassword(password) {
+                await this.inputPassword.setValue(password);
+            }
+
+            async clickLogin() {
+                await this.btnSubmit.click();
+            }
+
+            async getErrorMessage() {
+                //await this.errorMsg.waitForDisplayed();
+                return await this.errorMsg.getText();
+            }
+
+            async login(username, password) {
+                if (username !== undefined) await this.setUsername(username);
+                if (password !== undefined) await this.setPassword(password);
+                await this.clickLogin();
+            }
+
+            /**
+             * overwrite specific options to adapt it to page object
+             */
+            open() {
+                return browser.url(`https://www.saucedemo.com/`);
+            }
+        }
+
+        module.exports = new LoginPage();
         await this.btnSubmit.click();
     }
 
