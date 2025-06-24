b80a83ac0327b9fb973638708d0548c3a30796c8
EzgiUOztuna <ezgiuygn@gmail.com>
2025-06-24 13:55:50 +0300

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -22,53 +22,6 @@ class LoginPage extends Page {
     }
 
     async clickLogin() {
-        const { $ } = require('@wdio/globals')
-        const Page = require('./page');
-
-        /**
-         * sub page containing specific selectors and methods for a specific page
-         */
-        class LoginPage extends Page {
-            /**
-             * define selectors using getter methods
-             */
-            get inputUsername() { return $('#user-name'); }
-            get inputPassword() { return $('#password'); }
-            get btnSubmit() { return $('#login-button'); }
-            get errorMsg() { return $('.error-message-container'); }
-
-            async setUsername(username) {
-                await this.inputUsername.setValue(username);
-            }
-
-            async setPassword(password) {
-                await this.inputPassword.setValue(password);
-            }
-
-            async clickLogin() {
-                await this.btnSubmit.click();
-            }
-
-            async getErrorMessage() {
-                //await this.errorMsg.waitForDisplayed();
-                return await this.errorMsg.getText();
-            }
-
-            async login(username, password) {
-                if (username !== undefined) await this.setUsername(username);
-                if (password !== undefined) await this.setPassword(password);
-                await this.clickLogin();
-            }
-
-            /**
-             * overwrite specific options to adapt it to page object
-             */
-            open() {
-                return browser.url(`https://www.saucedemo.com/`);
-            }
-        }
-
-        module.exports = new LoginPage();
         await this.btnSubmit.click();
     }
 
